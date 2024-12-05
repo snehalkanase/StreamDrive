7ba2d8e1ce48b28a99b2e048d1565516b45ae1d8
Snehal Kanase <snehalkanase90@gmail.com>
2024-12-05 23:49:30 +0530

    updated api with production URL

--- frontend/src/utils/api.jsx
+++ frontend/src/utils/api.jsx
@@ -1,3 +1,3 @@
-// const api = "https://production.up.railway.app/api/";
-const api = "http://localhost:8080/api/";
+const api = "https://streamdrive.onrender.com";
+// const api = "http://localhost:8080/api/";
 export default api;
